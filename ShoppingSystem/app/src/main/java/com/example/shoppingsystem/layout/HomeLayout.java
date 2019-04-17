10df2ce43c5b59d0817d44e40df857a4760b37ee
jiang1124 <791246564@qq.com>
2019-04-17 16:27:38 +0800

    保存

--- ShoppingSystem/app/src/main/java/com/example/shoppingsystem/layout/HomeLayout.java
+++ ShoppingSystem/app/src/main/java/com/example/shoppingsystem/layout/HomeLayout.java
@@ -7,24 +7,21 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 
+
 import com.example.shoppingsystem.Application.BaseApplication;
 import com.example.shoppingsystem.R;
-import com.example.shoppingsystem.activity.MyAddressActivity;
-import com.example.shoppingsystem.adapter.AddressAdapter;
 import com.example.shoppingsystem.adapter.ProductListAdapter;
 import com.example.shoppingsystem.emtity.Product;
-import com.example.shoppingsystem.emtity.Recipient;
-import com.example.shoppingsystem.util.HttpUtil;
 import com.example.shoppingsystem.util.LogUtil;
 import com.example.shoppingsystem.util.ResponseUtil;
-import com.example.shoppingsystem.util.ToastUtil;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.Response;
 
 /**
@@ -42,54 +39,29 @@ public class HomeLayout extends LinearLayout {
         LayoutInflater.from(context).inflate(R.layout.home_layout,this);
 
         ButterKnife.inject(this);
-//        initProductList("http://10.0.2.2:8080//home");
-//        if(productList !=null ){
-//            GridLayoutManager layoutManager = new GridLayoutManager(BaseApplication.getContext(), 2);
-//            recyclerView.setLayoutManager(layoutManager);
-//            ProductListAdapter productListAdapter = new ProductListAdapter(productList);
-//            recyclerView.setAdapter(productListAdapter);
-//        }
-//        else {
-//            ToastUtil.makeText(BaseApplication.getContext(),"获取数据失败");
-//        }
-
+        initProductList("http://10.0.2.2:8080/home");
     }
 
     /*
      * 初始化
      */
-//    private void initProductList(String websiteAddress) {
-//        HttpUtil.sendOkHttpRequest(websiteAddress, new okhttp3.Callback() {
-//            @Override
-//            public void onResponse(okhttp3.Call call, Response response) throws IOException {
-//                final String responseText = response.body().string();
-//                final List<Product> productList = ResponseUtil.handleProductList(responseText);
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        if(productList !=null ){
-//                            GridLayoutManager layoutManager = new GridLayoutManager(BaseApplication.getContext(), 2);
-//                            recyclerView.setLayoutManager(layoutManager);
-//                            ProductListAdapter productListAdapter = new ProductListAdapter(productList);
-//                            recyclerView.setAdapter(productListAdapter);
-//                        }
-//                        else {
-//                            ToastUtil.makeText(BaseApplication.getContext(),"获取数据失败");
-//                        }
-//                    }
-//                });
-//            }
-//
-//            @Override
-//            public void onFailure(okhttp3.Call call, IOException e) {
-//                e.printStackTrace();
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        ToastUtil.makeText(BaseApplication.getContext(),"获取数据失败");
-//                    }
-//                });
-//            }
-//        });
-//    }
+    private void initProductList(String websiteAddress) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(websiteAddress)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String responseData = response.body().string();
+            LogUtil.d("jihao",responseData);
+            productList = ResponseUtil.handleProductList(responseData);
+            GridLayoutManager layoutManager = new GridLayoutManager(BaseApplication.getContext(), 2);
+            recyclerView.setLayoutManager(layoutManager);
+            ProductListAdapter productListAdapter = new ProductListAdapter(productList);
+            recyclerView.setAdapter(productListAdapter);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
 }
